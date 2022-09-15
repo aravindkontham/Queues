51dd303e67f27dbf3553b6865ca3a35739005d98
aravindkontham <lallithadevender11@gmail.com>
2022-09-15 15:43:07 +0530

    Palindromic using stacks:

--- src/pal.java
+++ src/pal.java
@@ -2,46 +2,36 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class pal {
-
-    static void Check(String n){
-        Stack<Character> st=new Stack<Character>();
-        Stack<Character> sn=new Stack<Character>();
-        char [] arr=n.toCharArray();
-
-        if(arr.length!=0)
-            for(int i=0;i<arr.length;i++){
-                st.push(arr[i]);
-            }
-
-
-        for(int j=arr.length-1;j>=0;j--){
+    static int check(char[] arr){
+        Stack<Character> st=new Stack<>();
+        Stack<Character> sn=new Stack<>();
+        for (int i=0;i<arr.length;i++){
+            st.push(arr[i]);
+        }
+        System.out.println("st:"+st);
+        for (int j=arr.length-1;j>=0;j--){
             sn.push(arr[j]);
         }
-
-        System.out.println(st);
-        System.out.println(sn);
-
-        if(st.equals(sn)){
-            System.out.println("it is a palindrome number");
-
-        }
-
-        else{
-            System.out.println("it's not a palindrome number");
-
-        }
-
-
+        System.out.println("sn:"+sn);
+        if(st.equals(sn))
+            return 1;
+        return -1;
 
     }
-
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter the string:");
         String str=scan.nextLine();
+         char[] arr=str.toCharArray();
+        //System.out.println(arr);
+        int res=check(arr);
+        //System.out.println(check(arr));
+        if(res==1)
+            System.out.println("Palindromic:");
+        else
+            System.out.println("Not a palindromic ");
 
 
-        Check(str);
     }
 }
